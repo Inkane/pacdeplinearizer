f391108bac6551330d7e9ab7953b44d459d331fe
Fabian Kosmale <0inkane@googlemail.com>
2015-04-06 18:24:44 +0200

    first working version

--- depend.cpp
+++ depend.cpp
@@ -3,19 +3,31 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <string>
 
 constexpr auto ROOTDIR = "/";
 constexpr auto dbpath = "/var/lib/pacman";
 
+using package = std::string;
+using dependency_map = std::unordered_map<package, std::vector<package>>;
+
 int main(int argc, char* argv[]) {
-  using package = std::string;
-  using dependency_map = std::unordered_map<package, std::vector<package>>;
+  std::ios_base::sync_with_stdio(false);
   auto packages = std::vector<package> {};
   auto pkg2deps = dependency_map {};
   auto pkg2optdeps = dependency_map {};
   alpm_errno_t err;
   auto handle = alpm_initialize(ROOTDIR, dbpath, &err);
+  if (!handle) {
+    std::cerr << "No cake for you:" << alpm_strerror(err) << std::endl;
+    return -1;
+  }
+  auto repos = {"core", "platform", "desktop", "apps", "games"};
+  for (const auto& repo : repos)    
+    alpm_register_syncdb(handle, repo, ALPM_SIG_USE_DEFAULT);
   alpm_list_t *dblist = alpm_get_syncdbs(handle);
+  if (!dblist)
+    std::cerr << "No dblists?!?" << std::endl;
   for (auto dbs = dblist;
        dbs;
        dbs = alpm_list_next(dbs)) {
@@ -30,27 +42,29 @@ int main(int argc, char* argv[]) {
 	   depends;
            depends = alpm_list_next(depends)
 	  ) {
-            auto dependency = reinterpret_cast<alpm_pkg_t*>(depends->data);
-	    std::string dependency_name = alpm_pkg_get_name(dependency);
+            auto dependency = reinterpret_cast<alpm_depend_t*>(depends->data);
+	    std::string dependency_name = dependency->name;
 	    pkg2deps[pkgname].push_back(dependency_name);
 	  }
       for (auto opt_depends = alpm_pkg_get_optdepends(pkg);
 	   opt_depends;
            opt_depends = alpm_list_next(opt_depends)
 	  ) {
-	    auto opt_dependency = reinterpret_cast<alpm_pkg_t*>(opt_depends->data);
-	    std::string dependency_name = alpm_pkg_get_name(opt_dependency);
+	    auto opt_dependency = reinterpret_cast<alpm_depend_t*>(opt_depends->data);
+	    std::string dependency_name = opt_dependency->name;
 	    pkg2optdeps[pkgname].push_back(dependency_name);
 	  }
     }
   }
   for (const auto& pkg: packages) {
     std::cout << pkg << ":\n";
-    std::cout << "\tdependencies \n";
+    std::cout << "\tdependencies: ";
     for (const auto& dependency: pkg2deps[pkg])
       std::cout << dependency << " ";
-    std::cout << "\toptional dependencies \n";
+    std::cout << "\n\toptional dependencies: ";
     for (const auto& opt_dependency: pkg2optdeps[pkg])
       std::cout << opt_dependency << " ";
+    std::cout << std::endl;
   }
+  return alpm_release(handle);
 }
